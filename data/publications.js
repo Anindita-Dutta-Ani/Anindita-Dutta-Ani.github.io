11a09b271278ddb545b22f58606d36925cbc208d
Anindita-Dutta-Ani <153369316+Anindita-Dutta-Ani@users.noreply.github.com>
2026-09-26 14:25:13 +0600

    Update publications.js

--- data/publications.js
+++ data/publications.js
@@ -1,7 +1,7 @@
 const PUBLICATIONS = [
   {
     authors: "<strong>Himika Tasnim; Angkon Dutta Joy; Anindita Dutta; Rawhatur Rabbi; Aniqua Nusrat Zereen</strong>",
-    title: " and LSTM Networks for Vision-Based Fall Detection Using Anatomical Keypoints",
+    title: " Temporal Fusion of Convolutional and LSTM Networks for Vision-Based Fall Detection Using Anatomical Keypoints",
     venue: "International Conference on Computer and Information Technology (ICCIT), 2025",
     links: [
       { label: "paper", url: "https://ieeexplore.ieee.org/document/11490414" },
